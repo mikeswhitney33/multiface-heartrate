a7fd990f81708a66e2b2049080f7c1245142e0fe
mikeswhitney33 <mikeswhitney33@gmail.com>
2019-03-26 15:46:00 -0600

    added support for multiple faces

--- application.py
+++ application.py
@@ -8,101 +8,221 @@ from kivy.graphics.texture import Texture
 import numpy as np
 
 import face_recognition
-import cv2
+import cv2 as cv
 import time
 
 from evm import evm
 
-class CamApp(App):
 
-    def build(self):
-        self.img1=Image()
-        layout = BoxLayout()
-        layout.add_widget(self.img1)
-        #opencv2 stuffs
-        self.capture = cv2.VideoCapture(0)
+"""
+Some Constants for using within the app
+"""
+RED = (0, 0, 255)
+WHITE = (255, 255, 255)
+FONT = cv.FONT_HERSHEY_DUPLEX
+RESCALE = 4
+
+
+class FaceQueue(object):
+    """
+    FaceQueue:
+    ----------
+    A class used to keep track of an individual face
+    """
+    def __init__(self, encoding, id=0):
+        """
+        method __init__:
+        ---------------
+        initializes a FaceQueue
+
+        Parameters:
+        @encoding: the encoding of the face we will use to compare with other faces.
+        @id: a unique identifier, mostly used for debuging purposes.
+        """
+        self.encoding = encoding
         self.queue = []
         self.times = []
         self.bpms = [0]
-        self.hr = 0
         self.t0 = time.time()
+        self.id = id
 
-        Clock.schedule_interval(self.update, 1.0/33.0)
+    def add(self, center, fps):
+        """
+        method add:
+        ----------
+        adds a frame to its queue and calculates heart rate
+
+        Parameters:
+        @center: the frame to add to its queue
+        @fps: the frames per second for calculating the heart rate
+        """
+        self.queue.append(center)
+        self.times.append(time.time() - self.t0)
+
+        if len(self.queue) > 10:
+            self.queue = self.queue[-100:]
+            self.times = self.times[-100:]
+            self.bpms = self.bpms[-100:]
+            frames = np.array(self.queue)
+
+            try:
+                hr = evm.find_heart_rate(frames, self.times, fps, .6, 1, alpha=20)
+
+                self.bpms.append(hr)
+            except:
+                pass
+
+    def get_hr(self):
+        """
+        method get_hr:
+        -------------
+        retrieves the heart rate from the queue
+
+        Returns:
+        the heart rate calculated from the queue
+        """
+        return np.mean(self.bpms)
+
+
+class CamApp(App):
+
+    def build(self):
+        """
+        method build:
+        ------------
+        sets up the UI of the App
+
+        Returns:
+        The root widget
+        """
+        layout = BoxLayout()
+        self.image = Image()
+        layout.add_widget(self.image)
+
+        self.capture = cv.VideoCapture(0)
+
+        self.queues = []
+
+        Clock.schedule_interval(self.update, 1/33)
         return layout
 
+    def get_queue(self, encoding, tolerence=0.6):
+        """
+        method get_queue:
+        ----------------
+
+        Parameters:
+        @encoding: the encoding of the face we are looking for
+        @tolerence: how similar two faces have to be to be considered the same.
+            Defaults to .6
+
+        Returns:
+        the index into the queue list if the face exists, -1 otherwise
+        """
+        for i, q in enumerate(self.queues):
+            diff = np.linalg.norm(encoding - q.encoding)
+            if diff < tolerence:
+                return i
+        return -1
+
     def write_frame(self, frame):
-        buffer = cv2.flip(frame, 0).tostring()
+        """
+        method write_frame:
+        ------------------
+        writes an image to the screen
 
-        texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
-        texture1.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
-        # display image from the texture
-        self.img1.texture = texture1
+        Parameters:
+        @frame: the frame we will be rendering
+        """
+        h, w, c = frame.shape
+        buf = frame[::-1].tostring()
 
-    def crop_center(self, img,cropx,cropy):
-        y,x, _ = img.shape
+        texture = Texture.create(size=(w, h), colorfmt='bgr')
+        texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+        self.image.texture = texture
+
+    def draw_heart_rate(self, frame, hr, top, right, bottom, left, id):
+        """
+        method draw_heart_rate:
+        ----------------------
+        draws the bounding box and heart rate on a frame
+
+        Parameters:
+        @frame: the frame for which we will do the drawing (is modified inplace)
+        @hr: the heart rate
+        @top, right, bottom, left: the extents of the bounding box
+        @id: an identifier for which face we are looking at, we could remove
+            this for final product
+        """
+        cv.rectangle(frame, (left, top), (right, bottom), RED, 2)
+        cv.rectangle(frame, (left, bottom - 35), (right, bottom), RED, cv.FILLED)
+        txt = "{}: {:.0f}".format(id, hr)
+        cv.putText(frame, txt, (left + 6, bottom - 6), FONT, 1, WHITE, 1)
+
+    def crop_center(self, im, size):
+        """
+        method crop_center:
+        ------------------
+        performs a center crop on an image
+
+        Parameters:
+        @im: the input image
+        @size: either a tuple (width, height) or int for uniform size
+
+        Returns:
+        The cropped image
+        """
+        y, x, _ = im.shape
+        if hasattr(size, '__len__'):
+            cropx, cropy = size
+        else:
+            cropx = size
+            cropy = size
         startx = x//2-(cropx//2)
-        starty = y//2-(cropy//2)    
-        return img[starty:starty+cropy,startx:startx+cropx,:]
+        starty = y//2-(cropy//2)
+        return im[starty:starty+cropy,startx:startx+cropx]
 
     def update(self, dt):
+        """
+        method update:
+        -------------
+        updates the app, this is called every 1/30 seconds
 
-        # Initialize some variables
-        face_locations = []
-
-        process_this_frame = True
-        # Grab a single frame of video
+        Parameters:
+        @dt: the elapsed time between this update and the last
+        """
+        # Read in frame
         ret, frame = self.capture.read()
 
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        # perform face detection on smaller frame for performance
+        small_frame = cv.resize(frame, (0, 0), fx=1/RESCALE, fy=1/RESCALE)
+        face_locations = face_recognition.face_locations(small_frame[:,:,::-1])
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
+        # get the face encodings to compare between faces
+        encodings = face_recognition.face_encodings(small_frame, face_locations)
 
-        # Only process every other frame of video to save time
-        if process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
+        for encoding, loc in zip(encodings, face_locations):
+            # rescale the bounding boxes
+            top, right, bottom, left = (a*RESCALE for a in loc)
+            # get the face from the frame
+            rect = frame[top:bottom,left:right]
+            # get the very center for more accurate heart rate
+            center = self.crop_center(rect, 80)
 
-        process_this_frame = not process_this_frame
+            fps = self.capture.get(cv.CAP_PROP_FPS)
+            # update the face queue
+            qi = self.get_queue(encoding)
+            if qi == -1:
+                q = FaceQueue(encoding, len(self.queues))
+                self.queues.append(q)
+            self.queues[qi].add(center, fps)
 
-        # Display the results
-        faces = []
-        for (top, right, bottom, left) in face_locations:
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
-
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            # faces.append(frame[top:bottom, left:right,:])
-
-            # self.queue.append([self.crop_center(np.array(frame[top:bottom, left:right,:]), 80, 80), time.time() - self.t0])
-            self.queue.append(self.crop_center(np.array(frame[top:bottom, left:right,:]), 80, 80))
-            self.times.append(time.time() - self.t0)
-
-
-            # if len(self.queue) >= self.capture.get(cv2.CAP_PROP_FPS)*5:
-            if len(self.queue) > 10:
-                self.queue = self.queue[-100:]
-                self.times = self.times[-100:]
-                self.bpms = self.bpms[-100:]
-                frames = np.array(self.queue)
-                
-
-                self.bpms.append(evm.find_heart_rate(frames, self.times, self.capture.get(cv2.CAP_PROP_FPS), 0.6, 1.0, alpha=20))
-
-            font = cv2.FONT_HERSHEY_DUPLEX
-
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            cv2.putText(frame, "{}".format(int(np.mean(self.bpms))), (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
+            hr = self.queues[qi].get_hr()
+            # draw the bounding box and heart rate on the frame
+            self.draw_heart_rate(frame, hr, top, right, bottom, left, self.queues[qi].id)
+        # display the frame
         self.write_frame(frame)
 
+
 if __name__ == '__main__':
-    # just_cv2()
     CamApp().run()
